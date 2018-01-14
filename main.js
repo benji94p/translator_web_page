57589c4d49c382b67b23043a341fe3e29094fd1b
Benjamin Pelzman <benjaminpelzman@MacBook-Pro-de-Benjamin.local>
2018-01-13 20:20:29 -0500

    firstd

---
+++ main.js
@@ -0,0 +1,12 @@
+/* WORD SWITCHER */
+
+let first = document.querySelector("#first");
+let second = document.querySelector("#second");
+
+function init () (
+    first.textContent = "French";
+)
+init();
+setInterval(function(){
+
+    ; }, 3000);
